d92b60709093de94e24729435454d8add54fe214
Adam Breaux <breauxa@email.chop.edu>
2015-06-24 11:37:17 -0400

    dep hell

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,18 +3,8 @@ class phpxcache {
     /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'php-xcache' ],
   }
 
-  $depends = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'php' ],
-  }
-
-  $service = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'httpd' ],
-  }
-
   package { $required:
     ensure  => latest,
-    require => Package[$depends],
-    notify  => Service[$service],
   }
 
 }
